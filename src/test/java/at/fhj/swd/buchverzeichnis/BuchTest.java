855f5ada648676f7453ca08bc17a64361bb52e13
[NaLaSWD] <nathalie.lanner@edu.fh-joanneum.at>
2018-04-15 18:18:25 +0200

    Buch-Genre fixed

--- src/test/java/at/fhj/swd/buchverzeichnis/BuchTest.java
+++ src/test/java/at/fhj/swd/buchverzeichnis/BuchTest.java
@@ -9,19 +9,19 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import static jdk.nashorn.internal.objects.Global.getDate;
 import static org.junit.Assert.*;
 
 @org.junit.FixMethodOrder( org.junit.runners.MethodSorters.NAME_ASCENDING)
 public class BuchTest {
 
     static GenreRepository genreRepository;static int gid = 97;
-    static String genreDescription = "Thriller";
-    static String genreUpdate = "Romane";
+    //static String genreDescription = "Thriller";
+    static String genreBezeichnung = "Thriller";
     static List<Buch> buecher = new ArrayList<>();
 
 
     static BuchRepository buchRepository;
+    static Buch buch;
     static final int bid = 158;
     static final int isbn = 5656;
     static Date erscheinungsjahr = new Date(20100822);
@@ -31,85 +31,86 @@ public class BuchTest {
     static List<Autor> autor = new ArrayList<>();
 
     @BeforeClass
-        public static void setup() {
+    public static void setup() {
 
-            genreRepository = new GenreRepository();
-            buchRepository = new BuchRepository();
-            Transaction.begin();
-            buchRepository.reset();
-            genreRepository.reset();
-            Transaction.commit();
-        }
+        genreRepository = new GenreRepository();
+        buchRepository = new BuchRepository();
+        Transaction.begin();
+        buchRepository.reset();
+        genreRepository.reset();
+        Transaction.commit();
+    }
 
-        @Test
-        public void create () {
-            Transaction.begin();
-            genre = genreRepository.create(gid, genreDescription);
-            buecher.add(buchRepository.create(bid, isbn, erscheinungsjahr, titel, genre));
-            Transaction.commit();
-        }
+    @Test
+    public void create () {
+        Transaction.begin();
+        genre = genreRepository.create(gid, genreBezeichnung);
+        buch = buchRepository.create(bid, isbn, erscheinungsjahr, titel, genre);
+        buecher.add(buch);
+        Transaction.commit();
+        assertEquals(genre, buch.getGenre());
+        assertTrue(genre.getBuecher().equals(buecher));
+    }
 
-        @Test
-        public void modify () {
-            Buch buch = buchRepository.find(bid);
-            assertNotNull(buch);
-            Transaction.begin ();
+    @Test
+    public void modify () {
+        buch = buchRepository.find(bid);
+        assertNotNull(buch);
+        Transaction.begin ();
 
-            buch.setIsbn(isbnUpdate);
-            Transaction.commit();
+        buch.setIsbn(isbnUpdate);
+        Transaction.commit();
 
-            buch = buchRepository.find(bid);
-            assertEquals(isbnUpdate, (int) buch.getIsbn());
-        }
+        buch = buchRepository.find(bid);
+        assertEquals(isbnUpdate, (int) buch.getIsbn());
+    }
 
-        @Test
-        public void findGenreBezeichnungByBuchTitel(){
-            String result = genreRepository.findGenreBezeichnung(titel);
-            assertEquals(genreDescription, result);
-        }
+    @Test
+    public void findGenreBezeichnungByBuchTitel(){
+        String result = genreRepository.findGenreBezeichnung(titel);
+        assertEquals(genreBezeichnung, result);
+    }
 
-        @Test
-        public void findBuecherByGenre(){
-            List<Buch> result = genreRepository.findBuecherByGenre(genreDescription);
-            assertEquals(buecher, result);
-        }
-
-
-        //Buch-Query Tests
-        @Test
-        public void findBuchTitelByErscheinungsjahr(){
-            String result = buchRepository.findBuchTitelByErscheinungsjahr(erscheinungsjahr);
-            assertEquals(titel, result);
-        }
-
-        @Test
-        public void findBuchIdByGenre(){
-            Integer result = buchRepository.findBuchIdByGenre(genreDescription);
-            assertEquals(buecher.get(0).getId(), (int)result);
-        }
+    @Test
+    public void findBuecherByGenre(){
+        List<Buch> result = genreRepository.findBuecherByGenre(genreBezeichnung);
+        assertEquals(buecher, result);
+    }
 
 
+    //Buch-Query Tests
+    @Test
+    public void findBuchTitelByErscheinungsjahr(){
+        String result = buchRepository.findBuchTitelByErscheinungsjahr(erscheinungsjahr);
+        assertEquals(titel, result);
+    }
 
-        @Test
-        public void remove () {
-            Buch buch = buchRepository.find(bid);
-            assertNotNull (buch);
-            Transaction.begin ();
+    @Test
+    public void findBuchIdByGenre(){
+        Integer result = buchRepository.findBuchIdByGenre(genreBezeichnung);
+        assertEquals(buecher.get(0).getId(), (int)result);
+    }
 
-            buchRepository.remove( buch );
-            Transaction.commit();
-            buch = buchRepository.find(bid);
-            assertNull (buch);
-        }
 
-        @AfterClass
-        public static void teardown() {
-            Transaction.begin();
-            buchRepository.reset();
-            genreRepository.reset();
-            Transaction.commit();
-        }
+
+    @Test
+    public void remove () {
+        buch = buchRepository.find(bid);
+        assertNotNull (buch);
+        Transaction.begin ();
+
+        buchRepository.remove( buch );
+        Transaction.commit();
+        buch = buchRepository.find(bid);
+        assertNull (buch);
+    }
+
+    @AfterClass
+    public static void teardown() {
+        Transaction.begin();
+        buchRepository.reset();
+        genreRepository.reset();
+        Transaction.commit();
+    }
 
 }
-
-
